8cf82d2533bb1d4170437a245dcf4ef61ea96222
Maria Luiza <maria.luiza.costa.balieiro@gmail.com>
2023-10-18 17:06:43 -0300

    em andamento

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+
 'use strict'
+
+const taylorInput = document.getElementById('taylor')
+ async function pegarMusica(taylor){
+    const url = `https://taylorswiftapi.onrender.com/get-all?song=<song>`
+    const response = await fetch(url)
+    const musica = await response.json()
+    return musica
+}
+
+taylorInput.addEventListener('focusout')
